e6905282b812340e2145c1bbee5b40a7177981ef
Kaluzny Adrien <kaluzny@2a02-8429-80d0-fd01-4223-43ff-fe7e-9d99.rev.sfr.net>
2025-01-14 10:39:10 +0100

    fix:styles

--- migrations/Version20250114093220.php
+++ migrations/Version20250114093220.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241219101429 extends AbstractMigration
+final class Version20250114093220 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241219101429 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, login VARCHAR(255) NOT NULL, pswd LONGTEXT NOT NULL, role VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD reset_token VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE `user`');
+        $this->addSql('ALTER TABLE user DROP reset_token');
     }
 }
